491f81b092c2f4d62f5e01fa603461ee5d11233c
luislimait <pls.luis@gmail.com>
2022-02-15 21:48:55 -0300

    Nested Routes

---
+++ src/pages/Products.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useNavigate, Outlet } from 'react-router';
+
+export default function Products() {
+
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <h1>
+                Products
+                <button onClick={() => navigate('/')}>Painel</button>
+            </h1>
+
+            <Outlet />
+        </>
+    )
+}
--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import {
     Routes,
     Route
 } from 'react-router-dom';
 
 import Dashboard from './Dashboard';
+import Products from './Products';
 import Profile from './Profile';
 
 export default function MainRoutes() {
@@ -12,6 +14,11 @@ export default function MainRoutes() {
         <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/profile/:id" element={<Profile />} />
+
+            <Route path="/products" element={<Products />} >
+                <Route path="purchases" element={<h1>Compras</h1>} />
+                <Route path="password" element={<h1>Alterar senha</h1>} />
+            </Route>
 
             <Route path="*" element={<h1>Not found =P</h1>} />
         </Routes>
