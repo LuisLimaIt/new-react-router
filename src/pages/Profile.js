35b387b39e423bd7567843f528292e5b7e2d96d0
luislimait <pls.luis@gmail.com>
2022-02-15 21:21:50 -0300

    useNavigate

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams , useNavigate } from 'react-router-dom';
 
 export default function Profile() {
     // useParams dá acesso ao id da rota
     const {id} = useParams();
+
+    const navigate = useNavigate();
+
     return (
-        <h1>Profile: {id}</h1>
+        <h1>
+            Profile: {id}
+            <button onClick={() => navigate('/')}>Painel</button>    
+        </h1>
     )
 }
